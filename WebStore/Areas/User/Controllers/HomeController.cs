7ee3d1f28fb96fab3546c907f0e187957cd5799e
Kári Kjærnested <kak25@hi.is>
2022-11-01 23:23:27 +0000

    Post IActionResult for details

--- WebStore/Areas/User/Controllers/HomeController.cs
+++ WebStore/Areas/User/Controllers/HomeController.cs
@@ -1,8 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.VisualBasic.Syntax;
 using Store.DataAccess.Repository.IRepository;
 using Store.Models;
 using System.Diagnostics;
-
+using System.Security.Claims;
 
 namespace WebStore.Areas.User.Controllers
 {
@@ -33,6 +36,21 @@ namespace WebStore.Areas.User.Controllers
                 Product = _unitOfWork.Product.GetFirstOrDefault(u => u.Id == productId, includeProperties: "Category,SubCategory")
         };
             return View(cart);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize]
+        public IActionResult Details(ShoppingCart shoppingCart)
+        { 
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+            shoppingCart.ApplicationUserId = claim.Value;
+            shoppingCart.Count = shoppingCart.Count + 1;
+            _unitOfWork.ShoppingCart.Add(shoppingCart);
+            _unitOfWork.Save();
+
+            return RedirectToAction(nameof(Index));
         }
 
         public IActionResult Privacy()
