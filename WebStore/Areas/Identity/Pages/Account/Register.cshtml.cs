3dfae759544881cf11231acd99f6ce640f66a251
Kári Kjærnested <kak25@hi.is>
2023-01-03 07:40:40 +0000

    Changes to user registration. Now only admin users can create users Employee or other admin users

--- WebStore/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ WebStore/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -199,7 +199,14 @@ namespace WebStore.Areas.Identity.Pages.Account
                     }
                     else
                     {
-                        await _signInManager.SignInAsync(user, isPersistent: false);
+                        if (User.IsInRole(SD.Role_Admin))
+                        {
+                            TempData["success"] = "New User Created Succesfully";
+                        }
+                        else
+                        {
+                            await _signInManager.SignInAsync(user, isPersistent: false);
+                        }
                         return LocalRedirect(returnUrl);
                     }
                 }
